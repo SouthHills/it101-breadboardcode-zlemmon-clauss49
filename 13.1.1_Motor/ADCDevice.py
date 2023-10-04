59efc4b13f2bce8a3083a90363d0ebf5e021f94d
sh-nicholaspage <npage@southhills.edu>
2023-10-04 16:09:35 -0400

    Added support for GRAVITECH ADCs

--- 13.1.1_Motor/ADCDevice.py
+++ 13.1.1_Motor/ADCDevice.py
@@ -30,7 +30,6 @@ class PCF8591(ADCDevice):
         
     def analogRead(self, chn): # PCF8591 has 4 ADC input pins, chn:0,1,2,3
         value = self.bus.read_byte_data(self.address, self.cmd+chn)
-        value = self.bus.read_byte_data(self.address, self.cmd+chn)
         return value
     
     def analogWrite(self,value): # write DAC value
@@ -43,5 +42,22 @@ class ADS7830(ADCDevice):
         self.address = 0x4b # 0x4b is the default i2c address for ADS7830 Module.   
         
     def analogRead(self, chn): # ADS7830 has 8 ADC input pins, chn:0,1,2,3,4,5,6,7
+        # Reads a byte of data from a hardware bus at a specific address using bitwise operations to construct the command to send based on the values of chn, self.cmd, and constants
         value = self.bus.read_byte_data(self.address, self.cmd|(((chn<<2 | chn>>1)&0x07)<<4))
         return value
+
+class GravitechADC(ADCDevice):
+    def __init__(self):
+        super(GravitechADC, self).__init__()
+        self.cmd = 0x84 #Use standard ADS command
+        self.address = 0x48 #Default address for Gravitech ADC (72)
+        
+    def analogRead(self, chn):
+        # Reads a byte of data from a hardware bus at a specific address using bitwise operations to construct the command to send based on the values of chn, self.cmd, and constants
+        value = self.bus.read_byte_data(self.address, self.cmd|(((chn<<2 | chn>>1)&0x07)<<4))
+        #Value can't be returned as is. Need to fetch MSB of 12-bit bus
+        msb = (value & 0xFF) << 4#FF is 16-bit mask. Shift 4 over to get 12
+        #LSB straight bussin'
+        lsb = (value & 0xF0) >> 4
+        #print("DEBUG\tValue: " + str(value) + "\tMSB: " + str(msb) + "\tLSB: " + str(lsb))
+        return (msb + lsb)
