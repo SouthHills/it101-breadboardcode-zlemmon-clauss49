0d50a956df9523aadfd95ec9f258d3ae3bba34e2
SH-NicholasPage <npage@southhills.edu>
2024-03-14 12:16:45 -0400

    Anotha one

--- 05_ColorfulLED/ColorfulLED.py
+++ 05_ColorfulLED/ColorfulLED.py
@@ -1,44 +1,40 @@
-import RPi.GPIO as GPIO
+from gpiozero import RGBLED
 import time
 import random
 
-pins = [11, 12, 13]         # define the pins for R:11,G:12,B:13 
+# active_high must be true because it is a common anode RGBLed
+led = RGBLED(red=17, green=18, blue=27, active_high=True)
 
-def setup():
-    global pwmRed,pwmGreen,pwmBlue  
-    GPIO.setmode(GPIO.BOARD)       # use PHYSICAL GPIO Numbering
-    GPIO.setup(pins, GPIO.OUT)     # set RGBLED pins to OUTPUT mode
-    GPIO.output(pins, GPIO.HIGH)   # make RGBLED pins output HIGH level
-    pwmRed = GPIO.PWM(pins[0], 2000)      # set PWM Frequence to 2kHz
-    pwmGreen = GPIO.PWM(pins[1], 2000)  # set PWM Frequence to 2kHz
-    pwmBlue = GPIO.PWM(pins[2], 2000)    # set PWM Frequence to 2kHz
-    pwmRed.start(0)      # set initial Duty Cycle to 0
-    pwmGreen.start(0)
-    pwmBlue.start(0)
+def set_color(r, g, b):
+    # invert the colors due to using a common anode
+    led.color = (1 - r, 1 - g, 1 - b)
 
-def setColor(r_val,g_val,b_val):      # change duty cycle for three pins to r_val,g_val,b_val
-    pwmRed.ChangeDutyCycle(r_val)     # change pwmRed duty cycle to r_val
-    pwmGreen.ChangeDutyCycle(g_val)   
-    pwmBlue.ChangeDutyCycle(b_val)
+def initial_test():
+    set_color(1, 0, 0)
+    print("All red")
+    time.sleep(1)
+    set_color(0, 0, 1)
+    print("All blue")
+    time.sleep(1)
+    set_color(0, 1, 0)
+    print("All green")
+    time.sleep(1)
 
 def loop():
     while True :
-        r=random.randint(0,100)  #get a random in (0,100)
+        r=random.randint(0,100)
         g=random.randint(0,100)
         b=random.randint(0,100)
-        setColor(r,g,b)          #set random as a duty cycle value 
-        print ('r=%d, g=%d, b=%d ' %(r ,g, b))
+        set_color(r / 100, g / 100, b / 100) # Colors should be between 0 and 1
+        print (f'r={r}% \tg={g}% \tb={b}%')
         time.sleep(1)
         
 def destroy():
-    pwmRed.stop()
-    pwmGreen.stop()
-    pwmBlue.stop()
-    GPIO.cleanup()
+    led.close()
     
 if __name__ == '__main__':     # Program entrance
     print ('Program is starting ... ')
-    setup()
+    initial_test()
     try:
         loop()
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
