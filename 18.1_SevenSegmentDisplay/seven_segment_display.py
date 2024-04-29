1309444d455f1f9984f6c3b30654aa82227b64a2
SH-NicholasPage <npage@southhills.edu>
2024-04-29 12:08:22 -0400

    Rewrote seven segement display. Can't think of how to simplify it. Will mull over it for a bit.

--- 18.1_SevenSegmentDisplay/seven_segment_display.py
+++ 18.1_SevenSegmentDisplay/seven_segment_display.py
@@ -1,16 +1,39 @@
-# Description : Control LED with 74HC595
-from gpiozero import OutputDevice # A generic GPIO output device that extends GPIODevice
+# Description : Control SevenSegmentDisplay with 74HC595
+import RPi.GPIO as GPIO
 import time
 
-# Defines the data bit that is transmitted preferentially in the shift_out function.
+from gpiozero import OutputDevice
+import time
+
 LSBFIRST = 1
 MSBFIRST = 2
-
 # define the pins for 74HC595
 DATA_PIN = OutputDevice(17)      # DS Pin of 74HC595(Pin14)
 LATCH_PIN = OutputDevice(27)     # ST_CP Pin of 74HC595(Pin12)
 CLOCK_PIN = OutputDevice(22)     # CH_CP Pin of 74HC595(Pin11)
-    
+
+# Define the patterns for the characters "0"-"F".
+# Due to common anode, 0 means LED on and 1 means LED off.
+# Note that the MSB is the decimal point.
+CHAR_PATTERNS = (
+    0b1100_0000, # 0
+    0b1111_1001, # 1
+    0b1010_0100, # 2
+    0b1011_0000, # 3
+    0b1001_1001, # 4
+    0b1001_0010, # 5
+    0b1000_0010, # 6
+    0b1111_1000, # 7
+    0b1000_0000, # 8
+    0b1001_0000, # 9
+    0b1000_1000, # A
+    0b1000_0011, # b
+    0b1100_0110, # C
+    0b1010_0001, # d
+    0b1000_0110, # E
+    0b1000_1110  # F
+)
+
 def shift_out(order,val):
     """
     Sends data to the 74HC595 chip using serial communication.
@@ -21,7 +44,6 @@ def shift_out(order,val):
         val (int): The data to be sent as a sequence of bits.
     """
     global LSBFIRST, MSBFIRST, CLOCK_PIN, DATA_PIN
-    debug_print_out: str = "["
     for i in range(0, 8):
         CLOCK_PIN.off()  # Turning off the clock to prepare for sending a bit
         if order == LSBFIRST:
@@ -29,53 +51,42 @@ def shift_out(order,val):
             #   and send it out accordingly
             if (0x01 & (val >> i) == 0x01):
                 DATA_PIN.on()   # If the bit is 1, we turn on the data pin
-                debug_print_out += "X, "
             else:
                 DATA_PIN.off()  # If the bit is 0, we turn off the data pin
-                debug_print_out += "O, "
         elif order == MSBFIRST:
             # If we're sending MSB first, we check each bit of the value from left to right
             #   and send it out accordingly
             if (0x80 & (val << i) == 0x80):
                 DATA_PIN.on()   # If the bit is 1, we turn on the data pin
-                debug_print_out += "X, "
             else:
                 DATA_PIN.off()  # If the bit is 0, we turn off the data pin
-                debug_print_out += "O, "
         CLOCK_PIN.on()  # Turning on the clock to send the bit
-    debug_print_out = debug_print_out[:-2] + "]"
-    print(debug_print_out)
 
 def loop():
-    """
-    This function controls the sequence of LED lighting using the 74HC595 chip.
-    """
     global LSBFIRST, MSBFIRST, LATCH_PIN
     while True:
-        x = 0x01  # Starting with the first LED lit
-        for _ in range(0, 8):
-            LATCH_PIN.off()         # Turning off the latch to prepare for updating data
-            shift_out(LSBFIRST, x)  # Sending the current LED pattern to the chip
-            LATCH_PIN.on()          # Turning on the latch to update the LED outputs
-            x <<= 1                 # Shifting the pattern to the left to light up the next LED
-            time.sleep(0.1)         # Waiting for a short time before the next LED
-        x = 0x80  # Starting with the last LED lit
-        for _ in range(0, 8):
-            LATCH_PIN.off()         # Turning off the latch to prepare for updating data
-            shift_out(LSBFIRST, x)  # Sending the current LED pattern to the chip
-            LATCH_PIN.on()          # Turning on the latch to update the LED outputs
-            x >>= 1                 # Shifting the pattern to the right to light up the previous LED
-            time.sleep(0.1)         # Waiting for a short time before the next LED
+        for i in range(0,len(CHAR_PATTERNS)):
+            print(hex(i))
+            LATCH_PIN.off()
+            shift_out(MSBFIRST,CHAR_PATTERNS[i])  # Send serial data to 74HC595
+            LATCH_PIN.on()
+            time.sleep(0.5)
+        for i in range(0,len(CHAR_PATTERNS)):
+            print(hex(i) + " .")
+            LATCH_PIN.off()
+            shift_out(MSBFIRST,CHAR_PATTERNS[i] & 0b0111_1111) # Bitwise AND to display the decimal point
+            LATCH_PIN.on()
+            time.sleep(0.5)
 
-def destroy():   
-    global DATA_PIN, LATCH_PIN, CLOCK_PIN
+def destroy():
+    global DATA_PIN, LATCH_PIN, CLOCK_PIN  
     DATA_PIN.close()
     LATCH_PIN.close()
-    CLOCK_PIN.close()
+    CLOCK_PIN.close() 
 
 if __name__ == '__main__': # Program entrance
     print ('Program is starting...' )
     try:
         loop()  
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
-        destroy()  
+        destroy()
