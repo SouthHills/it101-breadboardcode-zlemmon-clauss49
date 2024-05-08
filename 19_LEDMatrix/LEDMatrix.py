f7a1de271b3879a6b8eab41b5ce813e3d8711e3e
SH-NicholasPage <npage@southhills.edu>
2024-05-08 14:00:07 -0400

    I have no idea how to simplify this lol

--- 19_LEDMatrix/LEDMatrix.py
+++ 19_LEDMatrix/LEDMatrix.py
@@ -1,15 +1,15 @@
-# Description : Control LEDMatrix with 74HC595
-import RPi.GPIO as GPIO
+# Control LEDMatrix with 74HC595
+from gpiozero import OutputDevice
 import time
 
 LSBFIRST = 1
 MSBFIRST = 2
 # define the pins connect to 74HC595
-dataPin   = 11      # DS Pin of 74HC595(Pin14)
-latchPin  = 13      # ST_CP Pin of 74HC595(Pin12)
-clockPin = 15       # SH_CP Pin of 74HC595(Pin11)
-pic = [0x1c,0x22,0x51,0x45,0x45,0x51,0x22,0x1c]  # data of smiling face
-data = [     # data of "0-F"
+DATA_PIN   = OutputDevice(17)      # DS Pin of 74HC595(Pin14)
+LATCH_PIN  = OutputDevice(27)      # ST_CP Pin of 74HC595(Pin12)
+CLOCK_PIN  = OutputDevice(22)      # CH_CP Pin of 74HC595(Pin11)
+FACE = [0x1c,0x22,0x51,0x45,0x45,0x51,0x22,0x1c]  # data for smiling face
+DATA = [     # data of "0-F"
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, # " "
     0x00, 0x00, 0x3E, 0x41, 0x41, 0x3E, 0x00, 0x00, # "0"
     0x00, 0x00, 0x21, 0x7F, 0x01, 0x00, 0x00, 0x00, # "1"
@@ -29,50 +29,51 @@ data = [     # data of "0-F"
     0x00, 0x00, 0x7F, 0x48, 0x48, 0x40, 0x00, 0x00, # "F"
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, # " "
 ]
-def setup():
-    GPIO.setmode(GPIO.BOARD)    # use PHYSICAL GPIO Numbering
-    GPIO.setup(dataPin, GPIO.OUT)
-    GPIO.setup(latchPin, GPIO.OUT)
-    GPIO.setup(clockPin, GPIO.OUT)
     
-def shiftOut(dPin,cPin,order,val):
+def shiftOut(order,val):
+    global LSBFIRST, MSBFIRST, CLOCK_PIN
     for i in range(0,8):
-        GPIO.output(cPin,GPIO.LOW)
+        CLOCK_PIN.off()
         if(order == LSBFIRST):
-            GPIO.output(dPin,(0x01&(val>>i)==0x01) and GPIO.HIGH or GPIO.LOW)
+            DATA_PIN.on() if (0x01&(val>>i)==0x01) else DATA_PIN.off()
         elif(order == MSBFIRST):
-            GPIO.output(dPin,(0x80&(val<<i)==0x80) and GPIO.HIGH or GPIO.LOW)
-        GPIO.output(cPin,GPIO.HIGH)
+            DATA_PIN.on() if (0x80&(val<<i)==0x80) else DATA_PIN.off()
+        CLOCK_PIN.on()
 
 def loop():
+    global LATCH_PIN, MSBFIRST, DATA, FACE
     while True:
-        for j in range(0,500): # Repeat enough times to display the smiling face a period of time
+        for _ in range(0,500): # Repeat enough times to display the "smiling face" a period of time
             x=0x80
             for i in range(0,8):
-                GPIO.output(latchPin,GPIO.LOW)
-                shiftOut(dataPin,clockPin,MSBFIRST,pic[i]) #first shift data of line information to first stage 74HC959
+                LATCH_PIN.off()
+                shiftOut(MSBFIRST,FACE[i]) #first shift data of line information to first stage 74HC959
 
-                shiftOut(dataPin,clockPin,MSBFIRST,~x) #then shift data of column information to second stage 74HC959
-                GPIO.output(latchPin,GPIO.HIGH) # Output data of two stage 74HC595 at the same time
+                shiftOut(MSBFIRST,~x) #then shift data of column information to second stage 74HC959
+                LATCH_PIN.on()         # Output data of two stage 74HC595 at the same time
                 time.sleep(0.001) # display the next column
                 x>>=1
-        for k in range(0,len(data)-8): #len(data) total number of "0-F" columns 
-            for j in range(0,20): # times of repeated displaying LEDMatrix in every frame, the bigger the "j", the longer the display time.
+        for k in range(0,len(DATA)-8): #len(data) total number of "0-F" columns 
+            for _ in range(0,20): # times of repeated displaying LEDMatrix in every frame, the bigger the "j", the longer the display time.
                 x=0x80      # Set the column information to start from the first column
                 for i in range(k,k+8):
-                    GPIO.output(latchPin,GPIO.LOW)
-                    shiftOut(dataPin,clockPin,MSBFIRST,data[i])
-                    shiftOut(dataPin,clockPin,MSBFIRST,~x)
-                    GPIO.output(latchPin,GPIO.HIGH)
+                    LATCH_PIN.off()
+                    shiftOut(MSBFIRST,DATA[i])
+                    shiftOut(MSBFIRST,~x)
+                    LATCH_PIN.on()
                     time.sleep(0.001)
                     x>>=1
-def destroy():  
-    GPIO.cleanup()
+
+def destroy():
+    global DATA_PIN, LATCH_PIN, CLOCK_PIN
+    DATA_PIN.close()
+    LATCH_PIN.close()
+    CLOCK_PIN.close()
+
 if __name__ == '__main__':  # Program entrance
-    print ('Program is starting...' )
-    setup() 
+    print ('Program is starting...')
     try:
         loop()  
     except KeyboardInterrupt:   # Press ctrl-c to end the program.
-        destroy()  
-
+        destroy()
+        print("Ending program")
